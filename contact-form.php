a8f01dd7e1fbb13f1b804c28243bfaa839a9fd0f
Vinge1718 <vinge1718@gmail.com>
2018-06-18 13:33:48 +0300

    replaced the server source for email form

--- contact-form.php
+++ contact-form.php
@@ -3,7 +3,7 @@
     $visitor_email = $_Post['email'];
     $message = $_POST['message'];
 
-    $email_from = 'vnjonge@yahoo.com';
+    $email_from = 'https://vinge1718.github.io/';
     $email_subject = "New enquiry from Portfolio";
     $email_body = "User Name: $name.\n".
                     "User Email: $visitor_email.\n".
